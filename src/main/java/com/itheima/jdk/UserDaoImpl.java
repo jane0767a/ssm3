f304a238b397c904833d8f5d78dc9827409ccde2
jane0767a <jane0767a@163.com>
2018-01-11 15:10:11 +0800

    Commit

---
+++ src/main/java/com/itheima/jdk/UserDaoImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.jdk;
+
+import org.springframework.stereotype.Repository;
+
+//目标类
+@Repository("userDao")
+
+public class UserDaoImpl implements UserDao {
+
+	public void addUser() {
+		System.out.println("添加用户");
+
+	}
+
+	public void deleteUser() {
+		System.out.println("删除用户");
+
+	}
+
+}
